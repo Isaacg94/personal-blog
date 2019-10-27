1724c1f308edcb1535a106dc14e63e4cd048c599
Isaacg94 <7248zack@gmail.com>
2019-10-27 16:17:40 +0300

    add Quote class to ensure the Quote_test to pass

---
+++ app/models/quote.py
@@ -0,0 +1,10 @@
+class Quote:
+    '''
+    Quote class to define Quote Objects
+    '''
+
+    def __init__(self,author,id,quote,permalink):
+        self.author =author
+        self.id = id
+        self.quote = quote
+        self.permalink = permalink
