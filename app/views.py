f38f2a3509e827e325808371641388a719dd9b28
Isaacg94 <7248zack@gmail.com>
2019-10-27 16:34:03 +0300

    add view function to display random quotes in index route decorator

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_quotes
 
 # Views
 @app.route('/')
@@ -9,4 +10,5 @@ def index():
     View root page function that returns the index page and its data
     '''
     title = 'GZRU | Home'
-    return render_template('index.html', title=title)
+    random_quotes = get_quotes
+    return render_template('index.html', title=title, quotes=random_quotes)
