1bd7b6f99f4f5aac6366fc101a9234fbc88bc3f3
Isaacg94 <7248zack@gmail.com>
2019-10-27 16:13:34 +0300

    add quote_test file to test the Quotes class behaviour

---
+++ app/quotes_test.py
@@ -0,0 +1,21 @@
+import unittest
+from models import quote
+Quote = quote.Quote
+
+class QuoteTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Quote class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_quote = Quote("James O. Coplien",23,"You should name a variable using the same care with which you name a first-born child.","http://quotes.stormconsultancy.co.uk/quotes/23")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_quote,Quote))
+
+
+if __name__ == '__main__':
+    unittest.main()
