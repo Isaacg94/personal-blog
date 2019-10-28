8c1de1fa4284fe4f10959e5ac3866f2f15dfc61d
Isaacg94 <7248zack@gmail.com>
2019-10-28 10:06:34 +0300

    import flask security module and add methods to handle password hashing functionality

--- app/models.py
+++ app/models.py
@@ -53,6 +53,18 @@ class User(db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_secure = db.Column(db.String(255))
 
+    @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.password_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.password_secure,password)
+
     def __repr__(self):
         return f'User {self.username}'
 
