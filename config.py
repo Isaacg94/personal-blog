c0341b0d95de908f2d0ceb436bdb653013f8b31f
Isaacg94 <7248zack@gmail.com>
2019-10-28 12:05:43 +0300

    add email extension and initialize it

--- config.py
+++ config.py
@@ -8,6 +8,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://isaac:5272@localhost/blogposts'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='1234567890'
+export MAIL_USERNAME='gzrubot@gmail.com'
+export MAIL_PASSWORD='iG0715626693'
 
 python3.6 manage.py server
