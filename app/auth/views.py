ecd63d00ca1aacf8915085e4ec257f5391bc411d
Isaacg94 <7248zack@gmail.com>
2019-10-28 11:20:41 +0300

    add two columns in the users table to collect user bio and profile picture path

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from . import auth
 from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
+from .. import db
 
 
 @auth.route('/login',methods=['GET','POST'])
@@ -18,6 +19,17 @@ def login():
 
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 
 @auth.route('/logout')
