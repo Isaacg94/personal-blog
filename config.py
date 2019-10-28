94314dcf1b3958782e31bb98c2ddfcd305bbafe3
Isaacg94 <7248zack@gmail.com>
2019-10-28 08:56:00 +0300

    Create database and add user model to define the user data

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     '''
     QUOTE_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://isaac:5272@localhost/blogposts'
 
 
 
