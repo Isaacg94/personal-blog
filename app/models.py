372642b96a9d2f28867db2ced6dec29a4d472b3a
Isaacg94 <7248zack@gmail.com>
2019-10-28 10:03:40 +0300

    add password column in users table that stores passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
 class Comment:
 
     all_comments = []
@@ -48,6 +51,7 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
